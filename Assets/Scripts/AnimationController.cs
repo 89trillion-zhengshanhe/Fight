d44382a49bfa667610dd2898e98c581632b58c64
shanhe0921 <shanhe0522@outlook.com>
2021-05-21 11:42:25 +0800

    按照要求更新了动画控制逻辑

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("isAttack");
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger("isRun");
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            animator.SetTrigger("isIdle");
+        }
+    }
+}
